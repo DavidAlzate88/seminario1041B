4d6558ef80724b530317831ac36cda21524a5d6f
David Alzate <davids20xx@outlook.com>
2025-02-25 21:05:09 -0500

    feat: create project skeleton

---
+++ src/main/java/co/edu/uniajc/seminario1041b/Seminario1041BApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniajc.seminario1041b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminario1041BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminario1041BApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uniajc/seminario1041b/Seminario1041BApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniajc.seminario1041b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminario1041BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
